104eb1aff1eb5360e44524038b60e4aba9f2a309
Mahendra <mahendrapachipala123@gmail.com>
2023-11-30 22:04:46 +0530

    location update

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,48 +5,47 @@ import "../App.css";
 
 export default function Body({ city }) {
   const [data, setData] = useState(null);
-  let curlocation = ""; 
-  const[weather,setweather]=useState(null);
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    console.log("Geolocation not supported");
-  }
-
-  async function showPosition(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    try {
-      const location = await axios.get(
-        `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=e7cbec4493a84f6b89744cb2d587f992`
-      );
-      const village = location.data.results[0].components.village;
-      const state = location.data.results[0].components.state;
-      curlocation = village + "," + state;
-      const wea = city === "" ? curlocation : city;
-      setweather(wea);
-    } catch (error) {
-      console.error("Error fetching location data:", error);
-    }
-  }
- 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // Get geolocation
+        const position = await new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        const location = await axios.get(
+          `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=e7cbec4493a84f6b89744cb2d587f992`
+        );
+
+        const village = location.data.results[0].components.village;
+        const state = location.data.results[0].components.state;
+        const dist = location.data.results[0].components.state_district;
+        const curLocation = `${village},${dist},${state}`;
+        setWeather(city === "" ? curLocation : city);
+
+        // Fetch weather data
         const response = await axios.get(
           `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${weather}?unitGroup=metric&key=6BHXV54C28F5FRDLHZ8T46ARU&contentType=json`
         );
+
         setData(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setLoading(false);
       }
     };
 
     fetchData();
-  }, [weather]);
+  }, [weather, city]);
 
-  if (!data) {
+  if (loading) {
     return <div>Loading...</div>;
   }
 
@@ -62,12 +61,9 @@ export default function Body({ city }) {
 
       <div id="container">
         <h2 id="week">Weekly Forecast</h2>
-        <Days img={data.days[1].icon} climate={data.days[1].conditions} temp={data.days[1].temp} date = {data.days[1].datetime} />
-      <Days img={data.days[2].icon} climate={data.days[2].conditions} temp={data.days[2].temp} date = {data.days[1].datetime}/>
-      <Days img={data.days[3].icon} climate={data.days[3].conditions} temp={data.days[3].temp} date = {data.days[1].datetime}/>
-      <Days img={data.days[4].icon} climate={data.days[4].conditions} temp={data.days[4].temp} date = {data.days[1].datetime}/>
-      <Days img={data.days[5].icon} climate={data.days[5].conditions} temp={data.days[5].temp} date = {data.days[1].datetime}/>
-      <Days img={data.days[6].icon} climate={data.days[6].conditions} temp={data.days[6].temp} date = {data.days[1].datetime}/>
+        {data.days.slice(1,7).map((day) => (
+          <Days key={day.datetime} img={day.icon} climate={day.conditions} temp={day.temp} date={day.datetime} />
+        ))}
       </div>
     </div>
   );
